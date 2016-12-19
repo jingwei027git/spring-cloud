f441559bb0a041d7cce901ca30ee6b6a7d1ba3f4
david <jingwei027@gmail.com>
2016-12-19 12:22:30 +0800

    .

--- cloud-web-action/src/main/java/com/github027/WebActionApplication.java
+++ cloud-web-action/src/main/java/com/github027/WebActionApplication.java
@@ -2,12 +2,15 @@ package com.github027;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.client.RestTemplate;
 
+@EntityScan(basePackages = { "com.github027.domain.model" }, basePackageClasses = { Jsr310JpaConverters.class })
 @EnableRedisHttpSession
 @EnableDiscoveryClient
 @SpringBootApplication
